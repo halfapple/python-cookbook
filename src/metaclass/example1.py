eb7c9bd5adf269eacb691908ec6bb093f7fad8f3
wxl <123>
2020-04-13 09:55:50 +0800

    add magic methods

---
+++ src/metaclass/example1.py
@@ -0,0 +1,4 @@
+# __getattr__内置使用点号获取实例属性属性如 s.name，自调用__getattr__
+# __setattr__设置类实例属性 如s.name='tom'，自调用__setattr__
+# __getitem__ 使用[]获取实例属性 如s['name']，自调用__getitem__
+# __setitem__ 使用[]设置实例属性如 s['name'] = 'tom' ，自调用__setitem__
